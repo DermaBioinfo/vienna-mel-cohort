3b242bf22e7a62c621936977788731918d70caed
nttvy <nttv.2002@gmail.com>
2019-01-02 19:18:23 +0100

    change interface (one checkbox per gene, one dropdown list per gene), add V600E condition for BRAF mutation

--- twt_code.R
+++ twt_code.R
@@ -5,19 +5,19 @@ library("xlsx")
 mut <- c("BRAF", "NRAS", "NF1")
 
 s6 <- as.data.frame(read_excel("NIHMS393895-supplement-02.xlsx", sheet = "TABLE S6", skip = 5, col_names = T))
-s6 <- s6[, c(1, 2)]
+s6 <- s6[, c(1, 2, 9)]
 s6 <- subset(s6, grepl("ME0", s6[, 1]) & (s6[, 2] %in% mut))
-s6[, c("BRAF", "NRAS", "NF1")] <- c(grepl("BRAF", s6[, 2]), grepl("NRAS", s6[, 2]), grepl("NF1", s6[, 2]))
-s6[, 2] <- NULL
+s6[, c("BRAF", "NRAS", "NF1")] <- c(grepl("BRAF", s6[, 2]) & grepl("p.V600E", s6[, 3]), grepl("NRAS", s6[, 2]), grepl("NF1", s6[, 2]))
+s6[, c(2:3)] <- NULL
 s6 <- aggregate(s6, by=list(s6[, 1]), FUN = any)
 s6[, 2] <- NULL
 names(s6)[1] <- "Sample"
 
 s4 <- as.data.frame(read_excel("NIHMS362881-supplement-3.xlsx", sheet = "TABLE S4", skip = 5, col_names = T))
-s4 <- s4[, c(1, 2)]
+s4 <- s4[, c(1, 2, 20)]
 s4 <- subset(s4, grepl("ME0", s4[, 1]) & (s4[, 2] %in% mut))
-s4[, c("BRAF", "NRAS", "NF1")] <- c(grepl("BRAF", s4[, 2]), grepl("NRAS", s4[, 2]), grepl("NF1", s4[, 2]))
-s4[, 2] <- NULL
+s4[, c("BRAF", "NRAS", "NF1")] <- c(grepl("BRAF", s4[, 2]) & grepl("p.V600E", s4[, 3]), grepl("NRAS", s4[, 2]), grepl("NF1", s4[, 2]))
+s4[, c(2:3)] <- NULL
 s4 <- aggregate(s4, by=list(s4[, 1]), FUN = any)
 s4[, 2] <- NULL
 names(s4)[1] <- "Sample"
@@ -26,7 +26,8 @@ s10 <- as.data.frame(read_excel("NIHMS362881-supplement-3.xlsx", sheet = "TABLE 
 s10 <- s10[, c(1, 6, 7)]
 s10 <- subset(s10, grepl("ME0", s10[, 1]))
 s10[, 1] <- gsub("T", "", s10[,1])
-s10[, c(2, 3)] <- !is.na(s10[, c(2, 3)])
+s10[, 2] <- grepl("V600E", s10[, 2])
+s10[, 3] <- !is.na(s10[, 3])
 names(s10)[c(2, 3)] <- c("BRAF", "NRAS")
 s10$NF1 <- F
 
@@ -42,24 +43,26 @@ result$twt <- !(result$BRAF | result$NRAS | result$NF1)
 
 library("shiny")
 ui <- fluidPage(
-  titlePanel("Identification of mutated samples"),
+  titlePanel(strong("Identification of mutated samples")),
   
   sidebarLayout(
     sidebarPanel(
-      selectInput(inputId = "gene",
-                  label = "Choose a gene:",
-                  choices = c("BRAF", "NRAS", "NF1")),
-      
-      selectInput(inputId = "status",
-                  label = "Choose a status: ",
-                  choices = c("mutated", "non-mutated","ignored"))
+      h4("Genes and mutational status"),
+      h6("*Check the box to view the mutational status (mutated or non-mutated) of the gene"),
+      h6("*Uncheck the box for out-of-consideration gene"),
+      tags$br(),
+      lapply(mut, function(gene) {
+        fluidRow(
+          column(3, checkboxInput(inputId=toString(gene), label=toString(gene), TRUE)),
+          column(9, selectInput(inputId=paste0("status", toString(gene)), label = NULL, choices = c("mutated", "non-mutated"))))
+      })
     ),
     
     # Main panel for displaying outputs ----
-    mainPanel(
+    mainPanel(align="center",
       
       # Output: Formatted text for caption ----
-      h3(textOutput("Mutated samples", container = span)),
+      h4(strong("Mutational status")),
       
       # Output: HTML table
       tableOutput("view")
@@ -70,16 +73,30 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   datasetInput <- reactive({
-    gene <- toString(input$gene)
-    if (input$status == "mutated") {
-      subset(result, result[, gene] == TRUE)
-    } 
-    else if (input$status == "non-mutated") {
-      subset(result, result[, gene] == FALSE)
+    res <- result
+    for (gene in mut) {
+      if (input[[gene]]) {
+        if (input[[paste0("status", gene)]] == "mutated") {
+          temp <- subset(result, result[, toString(gene)] == TRUE)
+        } 
+        else {
+          temp <- subset(result, result[, toString(gene)] == FALSE)
+        }
+        res <- join(res, temp, type="inner", match = "all")
+      }
+      else {
+        # case of uncheck
+        res[, gene] <- NULL
+      }
     }
-    else {
-      result[, !names(result) %in% gene]
-    }
+    # another case of uncheck
+    #for (gene in mut) {
+      # we do not care about this gene (mutated or not)
+      #if (!input[[gene]]){
+        #res[, gene] <- NULL
+      #}
+    #}
+    res
   })
   
   output$view <- renderTable({
